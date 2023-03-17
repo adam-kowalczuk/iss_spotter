f2a5a5b0d467821e5630c60bc95caea81bb50a6a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-16 20:40:22 -0700

    Added code for converting date and output string

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  
+  console.log(printPassTimes(passTimes));
+});
+
+//VVV ORIGINAL FUNCTION CALLS
+
 // const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
@@ -28,13 +49,3 @@
 
 //   console.log("It worked! Returned Flyover Times:" , data);
 // });
-
-const { nextISSTimesForMyLocation } = require('./iss');
-
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  
-  console.log(passTimes);
-});

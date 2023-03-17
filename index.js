fb0305fd720f4cec790ee20bfb1a64d93e87de5d
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-16 20:02:20 -0700

    Imported nextISSTimesForMyLocation function and began formating it

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned IP:' , ip);
-// });
+  console.log('It worked! Returned IP:' , ip);
+});
 
-// fetchCoordsByIP('172.218.227.87', (error, data) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
+fetchCoordsByIP('172.218.227.87', (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
 
-//   console.log("It worked! Returned Geocode:" , data);
-// });
+  console.log("It worked! Returned Geocode:" , data);
+});
 
 const coords = { latitude: "48.4284207", longitude: "-123.3656444" };
 
@@ -27,4 +27,13 @@ fetchISSFlyOverTimes(coords, (error, data) => {
   }
 
   console.log("It worked! Returned Flyover Times:" , data);
+});
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
 });
